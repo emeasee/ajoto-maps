ce564f3cecd0bc705cdfe8823f04360333ce8e7b
Mac Oosthuizen <m@m-jo.co>
2017-05-01 15:16:54 +0200

    update properties

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,15 +14,28 @@ function flyToStore(currentFeature) {
   });
 }
 
-function createPopUp(currentFeature) {
+function createPopUpManufacturers(currentFeature) {
   var popUps = document.getElementsByClassName('mapboxgl-popup');
   // Check if there is already a popup on the map and if so, remove it
   if (popUps[0]) popUps[0].remove();
 
   var popup = new mapboxgl.Popup({ closeOnClick: false })
     .setLngLat(currentFeature.geometry.coordinates)
-    .setHTML('<h3>' + currentFeature.properties.title + '</h3>' +
+    .setHTML('<h3>' + currentFeature.properties.name + '</h3>' +
       '<h4>' + currentFeature.properties.description + '</h4>')
+    .addTo(map);
+}
+
+function createPopUpStockists(currentFeature) {
+  var popUps = document.getElementsByClassName('mapboxgl-popup');
+  // Check if there is already a popup on the map and if so, remove it
+  if (popUps[0]) popUps[0].remove();
+
+  var popup = new mapboxgl.Popup({ closeOnClick: false })
+    .setLngLat(currentFeature.geometry.coordinates)
+    .setHTML('<h3>' + currentFeature.properties.name + '</h3>' +
+      '<h4>' + currentFeature.properties.address + '</h4>' +
+      '<h4>' + currentFeature.properties.contact + '</h4>')
     .addTo(map);
 }
 
@@ -31,15 +44,22 @@ map.on('click', function(e) {
   // Query all the rendered points in the view
   if(stockists){
     var features = map.queryRenderedFeatures(e.point, { layers: ['stockists'] });
+    if (features.length) {
+      var clickedPoint = features[0];
+      // 1. Fly to the point
+      flyToStore(clickedPoint);
+      // 2. Close all other popups and display popup for clicked store
+      createPopUpStockists(clickedPoint);
+    }
   } else {
     var features = map.queryRenderedFeatures(e.point, { layers: ['manufacturers-commercial','manufacturers-sources','manufacturers-industrial','manufacturers-warehouses'] });
-  }
-  if (features.length) {
-    var clickedPoint = features[0];
-    // 1. Fly to the point
-    flyToStore(clickedPoint);
-    // 2. Close all other popups and display popup for clicked store
-    createPopUp(clickedPoint);
+    if (features.length) {
+      var clickedPoint = features[0];
+      // 1. Fly to the point
+      flyToStore(clickedPoint);
+      // 2. Close all other popups and display popup for clicked store
+      createPopUpManufacturers(clickedPoint);
+    }
   }
 });
 
